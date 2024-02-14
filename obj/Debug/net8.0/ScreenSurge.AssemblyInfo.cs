f816d12678c818efb566b426c707a3098cedd165
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-02-14 13:15:53 +0100

    Small change

--- obj/Debug/net8.0/ScreenSurge.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScreenSurge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSurge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6db8da659cbc7ffdfa990b729ab793f038a5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74643f4520e154b0ad1d2ef95ebc606447bcacc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSurge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSurge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
