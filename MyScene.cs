9c689c04b31fa60c033312480b6277acdd57447d
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-14 20:53:45 +0100

    Replaced prototype playerShip with Player object

--- MyScene.cs
+++ MyScene.cs
@@ -1,30 +1,22 @@
 ﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Raylib_cs;
 
 namespace ScreenSurge
 {
+
     class MyScene
     {
+        public const int screenWidth = 500;
+        public const int screenHeight = 500;
+        static public List<Bullet> bullets = new List<Bullet>();
         public static void Main()
         {
-            const int screenWidth = 500;
-            const int screenHeight = 500;
 
             //Making the window
             Raylib.InitWindow(screenWidth, screenHeight, "Screen Surge");
 
-            //Loading the player sprite
-            Texture2D texture = Raylib.LoadTexture("resources/playerShipSprite.png");
-
-            //Setting initial position
-            Vector2 playershipPosition = new Vector2(screenWidth / 2 - texture.Width / 2, screenHeight / 2 - texture.Height / 2);
-
-            float speed = 5.0f;
-
-            float scaleFactor = 1.5f;
-
-            //List with bullets
-            List<Bullet> bullets = new List<Bullet>();
+            Player playerShip = new Player(Raylib.GetMousePosition());
 
             //Loading in the texture for the bullet
             Texture2D bulletTexture = Raylib.LoadTexture("resources/bulletSprite.png");
@@ -34,24 +26,8 @@ namespace ScreenSurge
 
             while (!Raylib.WindowShouldClose())
             {
-                //Cursor position
-                Vector2 cursorPosition = Raylib.GetMousePosition();
-
-                //Movement
-                if (Raylib.IsKeyDown(KeyboardKey.W)) playershipPosition.Y -= speed;
-                if (Raylib.IsKeyDown(KeyboardKey.S)) playershipPosition.Y += speed;
-                if (Raylib.IsKeyDown(KeyboardKey.A)) playershipPosition.X -= speed;
-                if (Raylib.IsKeyDown(KeyboardKey.D)) playershipPosition.X += speed;
-
-                if (Raylib.IsMouseButtonPressed(MouseButton.Left))
-                {
-                    //Set direction
-                    Vector2 bulletDirection = Vector2.Normalize(new Vector2(cursorPosition.X - playershipPosition.X, cursorPosition.Y - playershipPosition.Y));
-
-                    //Spawn bullet, initial position, give direction
-                    bullets.Add(new Bullet(playershipPosition, bulletDirection, "resources/bulletSprite.png"));
-                }
-
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
                 //Update bullets
                 foreach (var bullet in bullets)
                 {
@@ -59,30 +35,14 @@ namespace ScreenSurge
                     bullet.Draw();
                 }
 
-
-
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.Black);
-
-                //Calculate ship angle to cursor
-                float angle = MathF.Atan2(cursorPosition.Y - playershipPosition.Y, cursorPosition.X - playershipPosition.X) * (180.0f / MathF.PI);
-
-
-                // Calculate the scaled width and height
-                int scaledWidth = (int)(texture.Width * scaleFactor);
-                int scaledHeight = (int)(texture.Height * scaleFactor);
-
-                // Draw the ship at its current position rotated by the angle
-                Raylib.DrawTexturePro(texture, new Rectangle(0, 0, texture.Width, texture.Height),
-                    new Rectangle((int)playershipPosition.X, (int)playershipPosition.Y, scaledWidth, scaledHeight),
-                    new Vector2(scaledWidth / 2, scaledHeight / 2), angle, Color.White);
-
+                playerShip.Update();
+                playerShip.Draw();
 
                 Raylib.EndDrawing();
             }
 
-            Raylib.UnloadTexture(texture);
             Raylib.UnloadTexture(bulletTexture);
+            playerShip.Destroy();
             Raylib.CloseWindow();
         }
     }
