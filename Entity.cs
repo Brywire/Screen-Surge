83c6446347b1f8dcb8a2bf84b24bcb7d669e7bbf
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-14 20:51:05 +0100

    Implemented Entity

--- Entity.cs
+++ Entity.cs
@@ -2,19 +2,56 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using Raylib_cs;
 
-public class Entity
+abstract public class Entity
 {
-    public Vector2 Position;
-    public Texture2D Texture;
+    protected Vector2 position;
+    protected Texture2D texture;
+    protected Vector2 direction;
+    protected float speed;
+    protected float scaleFactor;
 
-    public Entity(Vector2 position, string texturePath)
+    public Entity(Vector2 direction)
     {
-        Position = position;
-        Texture = Raylib.LoadTexture(texturePath);
+        this.direction = direction;
     }
 
-    public virtual void Update(float deltaTime)
+        public virtual float Angle
     {
+        get
+        {
+            return MathF.Atan2(direction.Y, direction.X) * (180.0f / MathF.PI);
+        }
+    }
 
+    public abstract void Update();
+   
+
+    public void Draw()
+    {
+        //Scaled texture
+        int scaledWidth = (int)(texture.Width * scaleFactor);
+        int scaledHeight = (int)(texture.Height * scaleFactor);
+
+        //Calculate the rectangle for the destination area considering the scaled dimensions
+        Rectangle destRec = new Rectangle(
+            (int)position.X - scaledWidth / 2,
+            (int)position.Y - scaledHeight / 2,
+            scaledWidth,
+            scaledHeight
+        );
+
+        //Draw the bullet texture with the scaling applied
+        Raylib.DrawTexturePro(
+            texture,
+            new Rectangle(0, 0, texture.Width, texture.Height),
+            destRec,
+            new Vector2(scaledWidth / 2, scaledHeight / 2), Angle, // Origin is now the center of the scaled image
+            Color.White
+        );
+    }
+    
+    public void Destroy()
+    {
+        Raylib.UnloadTexture(texture);
     }
 }
