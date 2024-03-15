e9f5f5ae4a81237d184e7f31cc2e843f8f320a6b
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-15 10:12:04 +0100

    Sorted loose Player code into structured methods

--- Player.cs
+++ Player.cs
@@ -6,15 +6,26 @@ class Player : Entity
 {
     public Player(Vector2 direction) : base(direction)
     {
-        //Loading the player sprite
+        // Loading the player sprite
         texture = Raylib.LoadTexture("resources/playerShipSprite.png");
         this.position = new Vector2(MyScene.screenWidth / 2 - texture.Width / 2, MyScene.screenHeight / 2 - texture.Height / 2);
 
-        speed = 5.0f;
-
+        // Setting speed and scaleFactor
+        speed = 6.0f;
         scaleFactor = 1.5f;
     }
-        public override float Angle
+
+    public override void Update()
+    {
+        // Cursor position
+        direction = Raylib.GetMousePosition();
+
+        Move();
+        Shoot();
+    }
+
+    // Rotate sprite to cursor
+    public override float Angle
     {
         get
         {
@@ -22,29 +33,33 @@ class Player : Entity
 
         }
     }
-
-    public override void Update()
+    public void Move()
     {
-        //Movement
-        if (Raylib.IsKeyDown(KeyboardKey.W)) position.Y -= speed;
-        if (Raylib.IsKeyDown(KeyboardKey.S)) position.Y += speed;
-        if (Raylib.IsKeyDown(KeyboardKey.A)) position.X -= speed;
-        if (Raylib.IsKeyDown(KeyboardKey.D)) position.X += speed;
+        Vector2 movement = new Vector2(0, 0);
 
-        //Cursor position
-        direction = Raylib.GetMousePosition();
+        if (Raylib.IsKeyDown(KeyboardKey.W)) movement.Y -= 1;
+        if (Raylib.IsKeyDown(KeyboardKey.S)) movement.Y += 1;
+        if (Raylib.IsKeyDown(KeyboardKey.A)) movement.X -= 1;
+        if (Raylib.IsKeyDown(KeyboardKey.D)) movement.X += 1;
 
-        if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+        if (movement != Vector2.Zero)
         {
-            //Set direction
-            Vector2 bulletDirection = Vector2.Normalize(new Vector2(direction.X - position.X, direction.Y - position.Y));
-
-            //Spawn bullet, initial position, give direction
-            MyScene.bullets.Add(new Bullet(position, bulletDirection, "resources/bulletSprite.png"));
+            movement = Vector2.Normalize(movement);
         }
 
+        movement *= speed;
 
+        position += movement;
     }
+    public void Shoot()
+    {
+        if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+        {
+            // Set bullet direction
+            Vector2 bulletDirection = Vector2.Normalize(new Vector2(direction.X - position.X, direction.Y - position.Y));
 
-
+            // Spawn bullet, initial position, give direction
+            MyScene.bullets.Add(new Bullet(position, bulletDirection, "resources/bulletSprite.png"));
+        }
+    }
 }
