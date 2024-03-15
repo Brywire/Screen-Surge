d62df0f0dbf1e8a46fa3629f1bdb09b6e5cddf2b
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-15 13:38:05 +0100

    Remade the Bullet class to now work with Entity

--- Bullet.cs
+++ Bullet.cs
@@ -1,61 +1,26 @@
 using System.Numerics;
 using Raylib_cs;
 
-class Bullet
+class Bullet : Entity
 {
-    public Vector2 Position;
-    public Vector2 Direction;
-    public Texture2D Texture;
-    public float Speed =  15.0f; //Bullet speed factor
-    private float scaleFactor =  1.4f; //Bullet scale factor
-
-    public Bullet(Vector2 position, Vector2 direction, string texturePath)
+    public Bullet(Vector2 direction, Vector2 startPosition) : base(direction)
     {
-        Position = position;
-        Direction = direction;
-        Texture = Raylib.LoadTexture(texturePath);
+        texture = Raylib.LoadTexture("resources/bulletSprite.png");
+        speed = 15.0f;
+        scaleFactor = 1.5f;
+        position = startPosition;
     }
 
-    public float Angle
+    public override float Angle
     {
         get
         {
-            return MathF.Atan2(Direction.Y, Direction.X) * (180.0f / MathF.PI);
+            return MathF.Atan2(direction.Y, direction.X) * (180.0f / MathF.PI);
         }
     }
 
-    public void Update()
+    public override void Update()
     {
-        Position += Direction * Speed;
-        /*
-        TODO
-            Remove bullet once is it off-screen for a couple seconds
-            Remove bullet once it hits an enemy
-        */
-    }
-
-    public void Draw()
-    {
-        //Scaled texture
-        int scaledWidth = (int)(Texture.Width * scaleFactor);
-        int scaledHeight = (int)(Texture.Height * scaleFactor);
-        
-        //Calculate the rectangle for the destination area considering the scaled dimensions
-        Rectangle destRec = new Rectangle(
-            (int)Position.X - scaledWidth /  2,
-            (int)Position.Y - scaledHeight /  2,
-            scaledWidth,
-            scaledHeight
-        );
-        
-        //Draw the bullet texture with the scaling applied
-        Raylib.DrawTexturePro(
-            Texture,
-            new Rectangle(0,  0, Texture.Width, Texture.Height),
-            destRec,
-            new Vector2(scaledWidth /  2, scaledHeight /  2), // Origin is now the center of the scaled image
-            Angle,
-            Color.White
-        );
+        position += direction * speed;
     }
 }
