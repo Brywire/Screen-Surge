8c6bd3e47ef9a58fc95c4edbc82eb4ac15c19fa2
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-15 13:47:53 +0100

    Implemented Enemy

--- Enemy.cs
+++ Enemy.cs
@@ -1,7 +1,20 @@
 using System.Numerics;
 using Raylib_cs;
+using ScreenSurge;
 
-class Enemy
+class Enemy : Entity
 {
+public Enemy() : base(new Vector2(0, 0)) // Assuming no initial direction for now
+    {
+        texture = Raylib.LoadTexture("resources/enemySprite.png");
+        speed = 5.0f; // Example speed
+        scaleFactor = 1.0f; // Example scale factor
+        position = new Vector2(MyScene.screenWidth / 2 - texture.Width / 2, MyScene.screenHeight / 2 - texture.Height / 2);
+    }
 
+    public override void Update()
+    {
+        // Example movement logic
+        //position.X += speed;
+    }
 }
