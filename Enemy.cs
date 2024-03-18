b750759bfde57383782395bfc9ac42a3e0c32b62
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-18 12:23:31 +0100

    Update on enemy comments

--- Enemy.cs
+++ Enemy.cs
@@ -4,17 +4,16 @@ using ScreenSurge;
 
 class Enemy : Entity
 {
-public Enemy() : base(new Vector2(0, 0)) // Assuming no initial direction for now
+public Enemy() : base(new Vector2(0, 0))
     {
         texture = Raylib.LoadTexture("resources/enemySprite.png");
-        speed = 5.0f; // Example speed
-        scaleFactor = 1.0f; // Example scale factor
+        speed = 5.0f;
+        scaleFactor = 1.0f;
         position = new Vector2(MyScene.screenWidth / 2 - texture.Width / 2, MyScene.screenHeight / 2 - texture.Height / 2);
     }
 
     public override void Update()
     {
-        // Example movement logic
         //position.X += speed;
     }
 }
