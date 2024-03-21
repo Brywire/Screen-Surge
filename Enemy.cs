0c4bb9bf2f20e60ef1309937c345b8137fd9d4b5
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-03-21 11:57:53 +0100

    Gave enemies a targetPosition

--- Enemy.cs
+++ Enemy.cs
@@ -4,11 +4,11 @@ using ScreenSurge;
 
 class Enemy : Entity
 {
-    Vector2 targetPosition;
-public Enemy() : base(new Vector2(0, 0))
+    public Vector2 targetPosition;
+    public Enemy() : base(new Vector2(0, 0))
     {
         texture = Raylib.LoadTexture("resources/enemySprite.png");
-        speed = 2.0f; // Set the speed for the enemy
+        speed = 3.0f; // Set the speed for the enemy
         scaleFactor = 1.0f;
         position = new Vector2(MyScene.screenWidth / 2 - texture.Width / 2, MyScene.screenHeight / 2 - texture.Height / 2);
 
@@ -17,13 +17,10 @@ public Enemy() : base(new Vector2(0, 0))
 
     public override void Update()
     {
-        // Get the player's current position
-       // Vector2 playerPosition = MyScene.playerShip.Position;
-
-        // Calculate the direction towards the player
-        Vector2 directionToPlayer = Vector2.Normalize(targetPosition - position);
+        // Calculate the direction towards the target
+        Vector2 directionToTarget = Vector2.Normalize(targetPosition - position);
 
         // Move the enemy towards the player
-        position += directionToPlayer * speed;
+        position += directionToTarget * speed;
     }
 }
