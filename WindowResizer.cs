ded1ea218b43c2ab6d99144b60c2e548bca97865
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-04-17 13:27:43 +0200

    [+] Documentation comments in WindowResizer

--- WindowResizer.cs
+++ WindowResizer.cs
@@ -28,8 +28,12 @@ public class WindowResizer
 
     private static readonly uint SWP_NOMOVE = 0x0002;
     private static readonly uint SWP_NOZORDER = 0x0004;
-    
-    // Method to shrink the window by a specified amount
+
+    /// <summary>
+    /// Method to shrink the window by a specified amount. 
+    /// </summary>
+    /// <param name="windowHandle">This is a handle to the window which needs to be resized.</param>
+    /// <param name="shrinkAmount">This is the amount by what the window size gets reduced by. </param>
     public static void ShrinkWindowBy(IntPtr windowHandle, int shrinkAmount)
     {
         GetWindowRect(windowHandle, out RECT rect);
@@ -39,6 +43,14 @@ public class WindowResizer
         SetWindowPos(windowHandle, IntPtr.Zero, 0, 0, newWidth, newHeight, SWP_NOMOVE | SWP_NOZORDER);
     }
 
+    /// <summary>
+    /// This function is designed to increase the size of a window by a specified amount,
+    /// and it can expand the window in different directions based on the BorderSide parameter.
+    /// </summary>
+    /// <param name="windowHandle"> This is a handle to the window which needs to be resized.</param>
+    /// <param name="expandAmount"> This is the amount by what the window size gets increased by.</param>
+    /// <param name="side"> This parameter determines the direction in which the window should be expanded. It can be one of the following values: Top, Right, Bottom, or Left.</param>
+    
     public static void ExpandWindowBy(IntPtr windowHandle, int expandAmount, BorderSide side)
     {
         GetWindowRect(windowHandle, out RECT rect);
